e30dea169b7d4ba69ce5f81837c0398045866a70
Mitch Johnson <Mitch Johnson>
2016-09-16 22:47:04 -0500

    allmemes are dank

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var requestify = require('requestify');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/:memeName',function(req,res,next)){
+router.get('/meme/:memeName',function(req,res,next){
 	var memeName = req.params.memeName;
-	var requestify = require('requestify');
-	requestify.get("https://memegen.link/" + memeName).then(function(response){
-		response.getBody();
-		console.log(reponse.body);
-	})
+	var requestName = 'https://memegen.link/templates/' + memeName;
+	requestify.get(requestName).then(function(response){
+		console.log(response.getBody());
+		res.send();
+		
+	});
 
 
-}
+});
+
+router.get('/allmemes',function(req,res,next){
+	var requestName = 'https://memegen.link/templates/';
+	requestify.get(requestName).then(function(response){
+		console.log(response.getBody());
+		res.send();
+	});
+});
 
 module.exports = router;
