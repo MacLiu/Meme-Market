41366a36c6d35bba49bdb2c089cc66731bb5392e
mliu <mliu@iastate.edu>
2016-09-16 22:47:05 -0500

    added documentation

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/:memeName',function(req,res,next)){
+router.get('/:memeName',function(req,res,next){
 	var memeName = req.params.memeName;
 	var requestify = require('requestify');
 	requestify.get("https://memegen.link/" + memeName).then(function(response){
 		response.getBody();
 		console.log(reponse.body);
-	})
-
-
-}
+	});
+});
 
 module.exports = router;
