285e38f607896dd2b2cf2ed02bfb6acd6fe3641f
Mitch Johnson <Mitch Johnson>
2016-09-17 13:21:11 -0500

    stuff

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,15 @@ router.get('/meme/:memeName',function(req,res,next){
 	var requestName = 'https://memegen.link/templates/' + memeName;
 	requestify.get(requestName).then(function(response){
 		var json = response.getBody();
-		//var imgURL = json.example;
+		var imgURL = json.example;
+		var first = imgURL.substring(0,20 + memeName.length());
+		console.log(first);
+		var second = imgURL.substring(21 + memeName.length(),imgURL.length());
+		var middle = '/_/_' + second;
+		var final = first + middle;
+		console.log(second);
+		console.log(middle);
+		console.log(final); 
 		res.send(json.example);
 
 		
