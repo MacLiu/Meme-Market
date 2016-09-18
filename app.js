8f2886504d5e9aa7936b9b9a75401f8dbff46779
Adam Heimendinger <adamhei@iastate.edu>
2016-09-18 13:17:15 -0500

    Created helper method for interval requests and added dependency for Cross-Orign Requests

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var routes = require('./routes/index');
 
 var app = express();
+
+var cors = require('express-cors');
+
+app.use(cors({
+    allowedOrigins: [
+        'localhost:4200', 'http://localhost:4200'
+    ]
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,17 +26,17 @@ app.set('view engine', 'jade');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -35,24 +44,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,137 +2,232 @@ var express = require('express');
 var router = express.Router();
 var requestify = require('requestify');
 var Clarifai = require('clarifai');
- 
+
 var Meme = require('../models/meme');
 
 var globalMemeName;
 
 Clarifai.initialize({
-  'clientId': 'AW_gcyJN5ff5VjdkXEblqdfRAYZXmM887WvngDaQ',
-  'clientSecret': 'U19JDAr5qNBKZ7NjWKYkIQrRubQt874GAwDIaC2E'
+    'clientId': 'AW_gcyJN5ff5VjdkXEblqdfRAYZXmM887WvngDaQ',
+    'clientSecret': 'U19JDAr5qNBKZ7NjWKYkIQrRubQt874GAwDIaC2E'
 });
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/meme/:memeName',function(req,res,next){
-	var memeName = req.params.memeName;
-	globalMemeName = memeName;
-	var requestName = 'https://memegen.link/templates/' + memeName;
-	requestify.get(requestName).then(function(response){
-		var json = response.getBody();
-		console.log(json.example);
-		var imgURL = json.example;
-		var first = imgURL.substring(0,21 + memeName.length);
-		var middle = '/_/_.jpg';
-		var final = first + middle;
-		Clarifai.getTagsByUrl(final).then(
-			function handleResponse(response0){
-			  var baseURL = 'https://www.reddit.com/r/adviceanimals/hot/.json?limit=100'
-			  requestify.get(baseURL).then(function(response){
-			  	var json = response.getBody();
-			  	var data = json.data;
-			  	var children = data.children;
-			  	var after = data.after;
-			  	var score = 0;
-			  	var matches = [];
-			  	for(var i = 0;i<children.length;i++){
-			  		var post = children[i];
-			  		var postdata = post.data;
-			  		if(!(postdata.is_self)){
-			  			var imgURL = postdata.url;
-			  			var filetype = imgURL.substring(imgURL.length-3,imgURL.length);
-			  			if(!(filetype === 'jpg'||filetype === 'png')){
-			  				imgURL = imgURL + '.jpg';
-			  			}
-			  			
-			  			matches[i] = Clarifai.getTagsByUrl(imgURL).then(
-			  				function handleResponse(response1) {
-			  					if (compareMemeObjects(response0, response1)) {
-			  						return true;
-			  					} else {
-			  						return false;
-			  					}
+router.get('/meme/:memeName', function (req, res, next) {
+    console.log("made it");
+    var memeName = req.params.memeName;
+    globalMemeName = memeName;
+    var requestName = 'https://memegen.link/templates/' + memeName;
+    requestify.get(requestName).then(function (response) {
+        var json = response.getBody();
+        console.log("here");
+        var imgURL = json.example;
+        var first = imgURL.substring(0, 21 + memeName.length);
+        var middle = '/_/_.jpg';
+        var final = first + middle;
+        Clarifai.getTagsByUrl(final).then(
+            function handleResponse(response0) {
+                console.log("don't betray me clarifai");
+                var baseURL = 'https://www.reddit.com/r/adviceanimals/hot/.json?limit=100';
+                requestify.get(baseURL).then(function (response) {
+                    console.log("and here");
+                    var json = response.getBody();
+                    var data = json.data;
+                    var children = data.children;
+                    var after = data.after;
+                    var score = 0;
+                    var matches = [];
+                    for (var i = 0; i < children.length; i++) {
+                        var post = children[i];
+                        var postdata = post.data;
+                        if (!(postdata.is_self)) {
+                            var imgURL = postdata.url;
+                            var filetype = imgURL.substring(imgURL.length - 3, imgURL.length);
+                            if (!(filetype === 'jpg' || filetype === 'png')) {
+                                imgURL = imgURL + '.jpg';
+                            }
 
-			  				},
-			  				handleError
-						);
-			  		}
-			  	}
-			  	for(var i = 0;i<matches.length;i++){
-					var post = children[i];
-					var postdata = post.data;
-					if (matches[i]) {
-						score += postdata.num_comments + postdata.score;
-					}
-				}
+                            matches[i] = Clarifai.getTagsByUrl(imgURL).then(
+                                function handleResponse(response1) {
+                                    if (compareMemeObjects(response0, response1)) {
+                                        return true;
+                                    } else {
+                                        return false;
+                                    }
 
-			  	var newMeme = new Meme({
-			 		'meme' : globalMemeName,
-			  		'score' : score,
-			  		'timestamp' : new Date().getTime()
-			  	});
-			  	Meme.createMeme(newMeme, function(error, resMeme) {
-			  		Meme.getHistoryByMeme(globalMemeName, function(err, his) {
-			  			if (err) {
-			  				console.log("error");
-			  			} else {
-			  				his.push(resMeme);
-			  				res.send(his);
-			  			}
-			  		});
-			  	});
-			  })
-			  .fail(function(response){
-			   
-			  });
-			},
-			handleError
-		);
+                                },
+                                handleError
+                            );
+                        }
+                    }
+                    for (var i = 0; i < matches.length; i++) {
+                        var post = children[i];
+                        var postdata = post.data;
+                        if (matches[i]) {
+                            score += postdata.num_comments + postdata.score;
+                        }
+                    }
 
-	})
-	.fail(function(response){
-		res.send(response.getCode());
-	});
+                    var newMeme = new Meme({
+                        'meme': globalMemeName,
+                        'score': score,
+                        'timestamp': new Date().getTime()
+                    });
+                    Meme.createMeme(newMeme, function (error, resMeme) {
+                        Meme.getHistoryByMeme(globalMemeName, function (err, his) {
+                            if (err) {
+                                console.log("error");
+                            } else {
+                                his.push(resMeme);
+                                console.log("Queried");
+                                res.send(his);
+                            }
+                        });
+                    });
+                })
+                    .fail(function (response) {
 
+                    });
+            },
+            handleError
+        );
 
+    })
+        .fail(function (response) {
+            res.send(response.getCode());
+        });
 });
 
-router.get('/allmemes',function(req,res,next){
-	var requestName = 'https://memegen.link/templates/';
-	requestify.get(requestName).then(function(response){
-		res.send(response.getBody());
-	});
+function busyWork(memeName, req, res, next) {
+    globalMemeName = memeName;
+    var requestName = 'https://memegen.link/templates/' + memeName;
+    requestify.get(requestName).then(function (response) {
+        var json = response.getBody();
+        var imgURL = json.example;
+        var first = imgURL.substring(0, 21 + memeName.length);
+        var middle = '/_/_.jpg';
+        var final = first + middle;
+        Clarifai.getTagsByUrl(final).then(
+            function handleResponse(response0) {
+                var baseURL = 'https://www.reddit.com/r/adviceanimals/hot/.json?limit=100';
+                requestify.get(baseURL).then(function (response) {
+                    var json = response.getBody();
+                    var data = json.data;
+                    var children = data.children;
+                    var after = data.after;
+                    var score = 0;
+                    var matches = [];
+                    for (var i = 0; i < children.length; i++) {
+                        var post = children[i];
+                        var postdata = post.data;
+                        if (!(postdata.is_self)) {
+                            var imgURL = postdata.url;
+                            var filetype = imgURL.substring(imgURL.length - 3, imgURL.length);
+                            if (!(filetype === 'jpg' || filetype === 'png')) {
+                                imgURL = imgURL + '.jpg';
+                            }
+
+                            matches[i] = Clarifai.getTagsByUrl(imgURL).then(
+                                function handleResponse(response1) {
+                                    if (compareMemeObjects(response0, response1)) {
+                                        return true;
+                                    } else {
+                                        return false;
+                                    }
+
+                                },
+                                handleError
+                            );
+                        }
+                    }
+                    for (var i = 0; i < matches.length; i++) {
+                        var post = children[i];
+                        var postdata = post.data;
+                        if (matches[i]) {
+                            score += postdata.num_comments + postdata.score;
+                        }
+                    }
+
+                    var newMeme = new Meme({
+                        'meme': globalMemeName,
+                        'score': score,
+                        'timestamp': new Date().getTime()
+                    });
+                    Meme.createMeme(newMeme, function (error, resMeme) {
+                        Meme.getHistoryByMeme(globalMemeName, function (err, his) {
+                            if (err) {
+                                console.log("error");
+                            } else {
+                                his.push(resMeme);
+                                if (res) {
+                                    res.send(his);
+                                }
+                            }
+                        });
+                    });
+                })
+                    .fail(function (response) {
+
+                    });
+            },
+            handleError
+        );
+
+    })
+        .fail(function (response) {
+            res.send(response.getCode());
+        });
+}
+
+router.get('/allmemes', function (req, res, next) {
+    var requestName = 'https://memegen.link/templates/';
+    requestify.get(requestName).then(function (response) {
+        res.send(response.getBody());
+    });
 });
 
-function handleError(err){
-  //console.log('promise error:', err);
-};
+// var interval = setInterval(function () {
+//     var requestName = 'https://memegen.link/templates/';
+//     requestify.get(requestName).then(function (response) {
+//         var memeMap = response.getBody();
+//         for (var key in memeMap){
+//             var urlArr = memeMap[key].split("/");
+//             var meme = urlArr[urlArr.length - 1];
+//             busyWork(meme, null, null, null);
+//         }
+//     });
+// }, 5000);
+
+function handleError(err) {
+    console.log('promise error:', err.status_msg);
+}
 
 function compareMemeObjects(meme1, meme2) {
-	var tags1 = meme1.results[0].result.tag.classes;
-	var tags2 = meme2.results[0].result.tag.classes;
+    var tags1 = meme1.results[0].result.tag.classes;
+    var tags2 = meme2.results[0].result.tag.classes;
 
 
-	var same = 0;
-	for (var i = 0; i < tags1.length; i++) {
-		for (var j = 0; j < tags2.length; j++) {
-			if (tags1[i] == tags2[j]){
-				same += 1;
-				break;
-			}
-		}
-	}
-	var ratio = same / tags1.length;
-	if (ratio > 0.5) {
-		return true;
-	} else {
-		return false;
-	}
-	
+    var same = 0;
+    for (var i = 0; i < tags1.length; i++) {
+        for (var j = 0; j < tags2.length; j++) {
+            if (tags1[i] == tags2[j]) {
+                same += 1;
+                break;
+            }
+        }
+    }
+    var ratio = same / tags1.length;
+    if (ratio > 0.5) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
 module.exports = router;
